800bf972a3ed6cdd11a19ca2936a542bd7364c71
Luis Torrealba <luisdavid25torrealba@gmail.com>
2021-07-27 21:55:58 -0400

    Creamos nuestros primeros Templates :)

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/Peliculas",
+    name: "Peliculas",
+    component: () => import(/* webpackChunkName: "Peliculas" */ '../views/Peliculas.vue')
+  },
+  {
+    path: '/Series',
+    name: 'Series',
+    component: () => import(/* webpackChunkName: "Series" */ '../views/Series.vue')
   }
 ]
 
---
+++ src/views/Series.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Series">
+    <h1>Aquí irán todas las Series</h1>
+  </div>
+</template>
