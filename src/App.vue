8a497bee9a5959fdd022182c06e56013d7c3ac59
Luis Torrealba <luisdavid25torrealba@gmail.com>
2021-07-29 19:31:45 -0400

    Consumo de api exitosa!

--- src/App.vue
+++ src/App.vue
@@ -6,47 +6,11 @@
       <router-link to="/Series">Series</router-link> |
     </div>
     <router-view/>
-
-    <div>
-        <div  id="app1">
-    <h1>Personajes</h1>
-    <button v-on:click="fetch">Consultar</button>
-    <div v-for="location of locations" v-bind:key="location.id">
-    {{ location }}
-  </div>
-  </div>
-    </div>
-    
   </div>
 </template>
 
 
 <script>
-import axios from 'axios';
-
-export default {
-  name: "App1" ,
-  data: function() {
-    return {
-      locations: []
-    };
-  },
-  methods:{
-    fetch() {
-      let result = axios.get("https://rickandmortyapi.com/api/location").then(res => {
-        this.locations = res.data.results;
-        console.log(res.data);
-        console.log(result);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    }
-  }
-
-    };
-
-
 
 
 </script>
--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -3,17 +3,39 @@
     <div class="hero is-white is-gradient is-bold">
       <div class="hero-body">
         <h1 class="title">
-          <span class="hs text success">Serie que vamosa mostrar</span>
+          <span class="hs text success">Personajes</span>
         </h1>
             <button class="button" v-on:click="fetch">Consultar</button>
       </div>
 
     </div>
-    hero.is-white.is-gradient.is-bold
-    <h1>Aquí irán todas las Peliculas</h1>
+
+      <div class="container">
+
+        <div class="column is-12-mobile is-4-desktop" v-for="character of characters" v-bind:key="character.id">
+          
+          <div class="card">
+            <div class="card-header"><img v-bind:src="character.image" v-bind:alt="character.name">
+            </div>
+
+          <div class="card-content">
+            
+            <h3 class="title">{{ character.name }}</h3>
+            <button class="button is-succes is-small">ver mas</button>
+
+          </div>
+
+        </div>
+
+      </div>
+
+  </div>
+
+    
+
     <div v-for="character of characters" v-bind:key="character.id">
     {{ character }}
-  </div>
+    </div>
   </div>
 
 </template>
